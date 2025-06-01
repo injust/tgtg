0ffabd7cb659d1f31581b3e6529f491d1b578b21
Justin Su <injustsu@gmail.com>
2025-06-01 14:16:53 -0400

    Add DataDome cookie extraction script

---
+++ .mitmproxy/scripts/datadome-cookie.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from http import HTTPStatus
+from http.cookiejar import Cookie, FileCookieJar, MozillaCookieJar
+from http.cookies import SimpleCookie
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import orjson as jsonlib
+from attrs import define, field
+from loguru import logger
+
+if TYPE_CHECKING:
+    from mitmproxy.http import HTTPFlow
+
+logger = logger.opt(colors=True)
+
+COOKIE_NAME = "datadome"
+COOKIES_PATH = Path.cwd() / "cookies.txt"
+
+
+@define(eq=False)
+class DataDomeCookie:
+    cookies: FileCookieJar = field(init=False, factory=lambda: MozillaCookieJar(COOKIES_PATH))
+    cookie: Cookie = field(init=False)
+
+    def __attrs_post_init__(self) -> None:
+        self.cookies.load()
+        self.cookie = next(cookie for cookie in self.cookies if cookie.name == COOKIE_NAME)
+
+    async def response(self, flow: HTTPFlow) -> None:
+        if flow.request.pretty_host == "api-sdk.datadome.co" and flow.response:
+            flow.response.status_code = HTTPStatus(flow.response.status_code)
+
+            if flow.response.status_code == HTTPStatus.OK and flow.response.content:
+                data = jsonlib.loads(flow.response.content)
+                match data["status"]:
+                    case HTTPStatus.OK:
+                        self.cookie.value = SimpleCookie(data["cookie"])[COOKIE_NAME].value
+                        logger.debug(self.cookie.value)
+                        self.cookies.save(str(COOKIES_PATH))
+                    case _:
+                        logger.error("{!r}<normal>: {}</normal>", flow.response.status_code, data)
+
+
+addons = [DataDomeCookie()]
